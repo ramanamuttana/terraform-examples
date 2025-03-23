520480909691bb13a71bb0bc2bd74069b62fa0bd
ramanamuttana <ramanamuttana@gmail.com>
2025-03-23 12:56:14 +0100

    Update on resource file

--- 02_secretfile/main.tf
+++ 02_secretfile/main.tf
@@ -1,10 +1,10 @@
 # simple file resource
 resource "local_file" "tf_example1" {
-  filename = "${path.module}/example-${count.index}.txt"
+  filename = "${path.module}/output/example-${count.index}.txt"
   content  = "ramana is a good instructor!"
   count = 1
 }
 resource "local_sensitive_file" "tf_example2" {
-  filename = "${path.module}/example-sensitive.txt"
+  filename = "${path.module}/output/example-sensitive.txt"
   content  = "ram is a good instructor!"
 }
