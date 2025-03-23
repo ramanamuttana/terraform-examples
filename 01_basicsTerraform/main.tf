afdcac179e5e2988481a11b74ec4d0b610676c98
ramanamuttana <ramanamuttana@gmail.com>
2025-03-23 12:51:59 +0100

    Output directory changed

--- 01_basicsTerraform/main.tf
+++ 01_basicsTerraform/main.tf
@@ -1,6 +1,6 @@
 # simple file resource
 resource "local_file" "tf_example1" {
-  filename = "${path.module}/example-${count.index}.txt"
+  filename = "${path.module}/output/example-${count.index}.txt"
   content  = "ramana is a good instructor!"
   count = 3
 }
